9b2909bc02b16816b0094942b7e43b77ca2eee11
黑猫 <56511062+cat991@users.noreply.github.com>
2021-12-10 02:53:45 +0800

    Update app.js

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -10,7 +10,6 @@ App({
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
         // env: 'my-env-id',
-        env: "env-1gpwd13dd723c683",
         traceUser: true,
       })
     }
